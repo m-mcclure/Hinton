6d08592dc635f1762d68dd81aa293c7a385c1d1d
lazersly <brandonjroberts@yahoo.com>
2015-05-19 13:40:26 -0700

    Adding restaurant detail views

---
+++ Hinton/RestaurantDetailCollectionViewController.h
@@ -0,0 +1,16 @@
+//
+//  RestaurantDetailCollectionViewController.h
+//  Hinton
+//
+//  Created by Brandon Roberts on 5/19/15.
+//  Copyright (c) 2015 BR World. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class MapPoint;
+
+@interface RestaurantDetailCollectionViewController : UICollectionViewController
+
+@property MapPoint *annotation;
+
+@end
--- Hinton/ViewController.m
+++ Hinton/ViewController.m
@@ -9,11 +9,14 @@
 #import "ViewController.h"
 #import "BackendService.h"
 #import "MapPoint.h"
+#import "RestaurantDetailCollectionViewController.h"
+#import "CloseBannerViewController.h"
 #import <MapKit/MapKit.h>
 
-@interface ViewController () <MKMapViewDelegate>
+@interface ViewController () <MKMapViewDelegate, CloseBannerDelegate>
 
 @property (strong, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) CloseBannerViewController *closeBanner;
 @property (strong, nonatomic) NSArray *mapPoints;
 
 @end
@@ -24,7 +27,8 @@ CLLocationDistance initialMapViewDistance = 5000;
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+  
+  self.closeBanner.delegate = self;
   
   self.mapView.delegate = self;
   self.mapPoints = [BackendService mapPointsForArea:CGRectZero];
@@ -34,10 +38,7 @@ CLLocationDistance initialMapViewDistance = 5000;
   point.coordinate = CLLocationCoordinate2DMake(47.622152, -122.312965);
   point.title = @"test";
   
-  [self.mapView setRegion:MKCoordinateRegionMakeWithDistance(point.coordinate, initialMapViewDistance, initialMapViewDistance) animated:YES];
-  
-//  [self.mapView addAnnotation:point];
-  [self.mapView addAnnotations:self.mapPoints];
+  [self.mapView setRegion:MKCoordinateRegionMakeWithDistance(point.coordinate, initialMapViewDistance, initialMapViewDistance) animated:NO];
 }
 
 -(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
@@ -50,12 +51,64 @@ CLLocationDistance initialMapViewDistance = 5000;
   
   if (!annoView) {
     annoView = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"mapAnnotation"];
-    annoView.image = [UIImage imageNamed:@"hinton_logo_annotation_size"];
+    annoView.image = [UIImage imageNamed:@"hinton_logo_map_pin_size"];
     annoView.canShowCallout = YES;
     annoView.draggable = NO;
+    annoView.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
   }
   
   return annoView;
 }
+
+-(void)mapViewDidFinishLoadingMap:(MKMapView *)mapView {
+  [self.mapView addAnnotations:self.mapPoints];
+}
+
+-(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control {  
+  
+  [self.view addSubview:self.closeBanner.view];
+  [self.closeBanner didMoveToParentViewController:self];
+  [self addChildViewController:self.closeBanner];
+  
+  self.closeBanner.view.frame = CGRectMake(0, self.view.frame.size.height, self.view.frame.size.width, 50);
+  
+  [UIView animateWithDuration:0.5 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+    self.closeBanner.view.frame = CGRectMake(0, self.mapView.frame.origin.y, self.closeBanner.view.frame.size.width, self.closeBanner.view.frame.size.height);
+  } completion:^(BOOL finished) {
+    self.closeBanner.isOnscreen = YES;
+  }];
+  
+}
+
+//-(void)mapViewDidFinishRenderingMap:(MKMapView *)mapView fullyRendered:(BOOL)fullyRendered {
+//  [self.mapView addAnnotations:self.mapPoints];
+//}
+
+//-(void)mapViewWillStartLoadingMap:(MKMapView *)mapView {
+//  [self.mapView addAnnotations:self.mapPoints];
+//}
+//
+//-(void)mapViewWillStartRenderingMap:(MKMapView *)mapView {
+//  [self.mapView addAnnotations:self.mapPoints];
+//}
+
+#pragma mark - CloseBannerDelegate
+
+-(void)closeButtonPressed {
+  //TODO: Dismiss the views
+}
+
+#pragma mark - Custom Property Getters/Setters
+
+-(CloseBannerViewController *)closeBanner {
+  if (_closeBanner) {
+    return _closeBanner;
+  }
+  
+  _closeBanner = [self.storyboard instantiateViewControllerWithIdentifier:@"CloseBannerVC"];
+  return _closeBanner;
+
+}
+
 
 @end
